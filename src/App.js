31b947879340d35dc723b90fad380236a5e60bf9
Nathaniyell <essien.nathan@yahoo.com>
2023-10-09 04:08:15 +0100

    corrected typos and update CC component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import UserFinder from './components/UserFinder';
-import UsersContext from './store/users-context';
+import UserFinder from './components/ClassBasedUserFinder';
+import UsersContext from "./store/users-context";
 
 const DUMMY_USERS = [
   { id: 'u1', name: 'Max' },
--- src/components/ClassBasedUserFinder.js
+++ src/components/ClassBasedUserFinder.js
@@ -1,31 +1,30 @@
 import React, { Component } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
+import UsersContext from "../store/users-context";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
 class UserFinder extends Component {
+  static contextType = UsersContext; //imports the context to be accessed by this component
   constructor() {
     super();
     this.state = {
-      fileredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
   }
-
+componentDidMount(){
+  this.setState({ filteredUsers: this.context.users})
+}
   componentDidUpdate(prevProps, prevState){
 if(prevState.searchTerm !== this.state.searchTerm){
     this.setState({
-        filteredUsers:DUMMY_USERS.filter(user=>user.name.includes(this.state.searchTerm))
+      filteredUsers:this.context.users.filter(user=>user.name.includes(this.state.searchTerm))
     })
 }
   //componentDidUpdate provides us with two parameters which allows us to check if the state of a particular property was changed before the effect is rerun so as not to create an infinite loop and rerun the effect each time any part of this component is changed
   }
 
-  serchChangeHandler(event) {
+  searchChangeHandler(event) {
     this.setState({
       searchTerm: event.target.value,
     });
@@ -34,10 +33,11 @@ if(prevState.searchTerm !== this.state.searchTerm){
   render() {
     return (
       <>
+
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.filteredUsers} />
+        <Users users={this.state.filteredUsers} />
       </>
     );
   }
